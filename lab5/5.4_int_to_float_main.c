fba638c41b0fb8ee0d8aebf7a429c5d658b5f5b3
Igor Józefowicz <igorjozefowicz@gmail.com>
2023-12-14 07:10:27 +0100

    add lab5: 5.3, 5.4, 5.5, 5.6 solutions

---
+++ lab5/5.4_int_to_float_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void int2float(int* calkowite, float* zmienno_przec);
+
+int main() {
+	int tablicaCalkowite[2] = { 4, 5 };
+	float tablicaZmienno[2];
+
+	int2float(tablicaCalkowite, tablicaZmienno);
+
+	printf("\nKonwersja = %f %f\n", tablicaZmienno[0], tablicaZmienno[1]);
+
+	return 0;
+}
